e426c856a65e8767c034f90350a678414bf51821
gibsonogram <gibsonadamsc@gmail.com>
2021-04-21 20:06:32 -0700

    hell fuckin naahhhh

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
+import websocket, json, pprint
 import numpy as np
 import pandas as pd
 
-data = [[1,2,3],
-        [3,4,2],
-        [1,1,1]]
 
-fred = pd.DataFrame(data)
+coin = 'btcusd'
+c_time = '1m'
 
-love = np.arange(1,20)
+SOCKET = f'wss://stream.binance.us:9443/ws/{coin}@kline_{c_time}'
 
-print(love, data)
+def on_open(ws):
+        print('connection opened')
+
+def on_close(ws):
+        print('connection closed')
+
+def on_message(ws, message):
+        json_message = json.loads(message)
+        pprint.pprint(json_message)
+
+def on_error(ws, error):
+        print(error)
+
+ws = websocket.WebSocketApp(SOCKET,
+                            on_open = on_open,
+                            on_error = on_error,
+                            on_message = on_message,
+                            on_close = on_close
+                            )
+
+ws.run_forever()
+
