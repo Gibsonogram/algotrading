035f01a05e838e1bf7ccf469e38563091cbc9f28
gibsonogram <gibsonadamsc@gmail.com>
2021-05-01 11:36:54 -0700

    the stategy for high market cap coins is trash. I need to backtest with low market cap coins, and rethink strategy for well known coins.

---
+++ backtesting_low_MC_coins.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+#clean the data
+ATOM = pd.read_csv('historical_data/ATOM_hist_data.csv')
+OMG = pd.read_csv('historical_data/Bitfinex_OMGUSD_1h.csv')
+REP = pd.read_csv('historical_data/Bitfinex_REPUSD_1h.csv')
+MATIC = pd.read_csv('historical_data/Historical Data_MATIC.csv')
+HBAR = pd.read_csv('historical_data/HistoricalData_HBAR.csv')
+XTZ = pd.read_csv('historical_data/HistoricalData_XTZ.csv')
+IOTA = pd.read_csv('historical_data/IOTA_hist_data.csv')
+ZRX = pd.read_csv('historical_data/Poloniex_ZRXUSDT_30min.csv')
+
