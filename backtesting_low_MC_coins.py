d53d3cc94f0c7cdcaa5bac813929d413464a4a08
gibsonogram <gibsonadamsc@gmail.com>
2021-05-01 17:02:34 -0700

    So I added and cleaned a buttload of low marketcap data

--- backtesting_low_MC_coins.py
+++ backtesting_low_MC_coins.py
@@ -2,12 +2,15 @@ import pandas as pd
 
 
 #clean the data
-ATOM = pd.read_csv('historical_data/ATOM_hist_data.csv')
-OMG = pd.read_csv('historical_data/Bitfinex_OMGUSD_1h.csv')
-REP = pd.read_csv('historical_data/Bitfinex_REPUSD_1h.csv')
-MATIC = pd.read_csv('historical_data/Historical Data_MATIC.csv')
-HBAR = pd.read_csv('historical_data/HistoricalData_HBAR.csv')
-XTZ = pd.read_csv('historical_data/HistoricalData_XTZ.csv')
-IOTA = pd.read_csv('historical_data/IOTA_hist_data.csv')
-ZRX = pd.read_csv('historical_data/Poloniex_ZRXUSDT_30min.csv')
+ATOM = pd.read_csv('historical_data/ATOM_daily.csv')
+OMG = pd.read_csv('historical_data/OMG_hourly.csv')
+REP = pd.read_csv('historical_data/REP_hourly.csv')
+MATIC = pd.read_csv('historical_data/MATIC_daily.csv')
+HBAR = pd.read_csv('historical_data/HBAR_daily.csv')
+XTZ = pd.read_csv('historical_data/XTZ_daily.csv')
+IOTA = pd.read_csv('historical_data/IOTA_daily.csv')
+ZRX = pd.read_csv('historical_data/ZRX_hourly.csv')
+ETC = pd.read_csv('historical_data/ETC_hourly.csv')
+
+
 
